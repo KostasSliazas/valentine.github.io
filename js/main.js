8b917790e88249ac16b107d31fe4406ab88841eb
Kostas <kostas.sliazas@gmail.com>
2023-02-16 23:25:27 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,8 +17,12 @@
   const timers = d.getElementById('timers')
   // get clicks element
   const clicks = d.getElementById('clicks')
+  //get timer elements
+  const minutes = d.getElementById('minutes')
+  const seconds = d.getElementById('seconds')
   // card object
   const card = {
+    timer: 0,
     data: null,
     card: null,
     clicks: 0,
@@ -45,41 +49,16 @@
     return that
   }
 
-
-  // global variable for timer function
-  let on = 0
-
   function timer() {
     let sec = 0
 
     function pad(val) {
       return val > 9 ? val : '0' + val
     }
-    on = setInterval(function () {
-      d.getElementById('seconds').innerHTML = pad(++sec % 60)
-      d.getElementById('minutes').innerHTML = pad(parseInt(sec / 60, 10))
+    card.timer = setInterval(function () {
+      seconds.innerHTML = pad(++sec % 60)
+      minutes.innerHTML = pad(parseInt(sec / 60, 10))
     }, 1000)
-  }
-
-
-  /**
-   * @suppress {missingProperties|checkTypes}
-   */
-  function start() {
-    shuffleCards = getShuffledArr(arr.concat(arr))
-    shuffleCards.forEach(e => {
-      const ele = new Elements('DIV', 'wrpko', '')
-      const kor = new Elements('DIV', 'korta', '')
-      const gal = new Elements('DIV', 'galas ' + e, '')
-      const pri = new Elements('DIV', 'priekis', '')
-      ele.appendChild(kor).appendChild(gal)
-      kor.appendChild(pri)
-      documentFragment.appendChild(ele)
-    })
-    gridContainer.appendChild(documentFragment)
-    timer()
-    // show clicks (moves)
-    clicks.innerHTML = card.clicks
   }
 
 
@@ -105,6 +84,7 @@
       if (!card.cards) card.card = e.target
       if (card.cards === 2) return false
         ++card.cards
+      // add random class name to rotate direction
       const randomClass = Math.round(Math.random()) >= 0.5 ? 'pasukti' : 'pasukti1'
       target.className += ' ' + randomClass
 
@@ -118,15 +98,14 @@
         gridContainer.style.pointerEvents = 'none'
 
         if (card.card.firstChild.className === target.firstChild.className) {
-          let inter0 = setTimeout(() => {
-            clearTimeout(inter0)
-            inter0 = 0
+          setTimeout(() => {
+
             target.classList.add('blur')
             card.card.classList.add('blur')
             card.cards = 0
             gridContainer.style.pointerEvents = 'auto'
             if (gridContainer.getElementsByClassName('blur').length === shuffleCards.length) {
-              clearInterval(on)
+              clearInterval(card.timer)
               message.innerHTML = `<h2>Your time: ${timers.textContent}</h2>`
 
               // first set data only then get to show it
@@ -184,10 +163,33 @@
     }
   }
 
+  /**
+   * @suppress {missingProperties|checkTypes}
+   */
+  function start() {
+    shuffleCards = getShuffledArr(arr.concat(arr))
+    shuffleCards.forEach(e => {
+      const ele = new Elements('DIV', 'wrpko', '')
+      const kor = new Elements('DIV', 'korta', '')
+      const gal = new Elements('DIV', 'galas ' + e, '')
+      const pri = new Elements('DIV', 'priekis', '')
+      ele.appendChild(kor).appendChild(gal)
+      kor.appendChild(pri)
+      documentFragment.appendChild(ele)
+    })
+    gridContainer.appendChild(documentFragment)
+    // show the timer
+    timer()
+    //just to show zero seconds and minutes
+    minutes.innerHTML = seconds.innerHTML = '00'
+    // show clicks (moves)
+    clicks.innerHTML = card.clicks
+  }
+
   function init() {
-    // set data array on constant variable, for later use
+    // set data for later use
     card.data = getData()
-    // show data if data length is more than 0
+    // show data if data length is more than 1
     showData()
     // create the start button with text 'Start'
     button('Start')
